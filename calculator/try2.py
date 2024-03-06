005cbcaa0efc0d72113a5223b74805b5818f25bf
CharmiPanchal17 <charmipanchal17@gmail.com>
2024-03-06 21:47:16 +0300

    calculator using lambda and numpy

--- calculator/try2.py
+++ calculator/try2.py
@@ -1,54 +1,53 @@
+import numpy as np
 
 class Calculator:
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
+    def __init__(self, action) -> None:
+        self.action = action
+    def calculate(self, num1, num2):
+        return self.action(num1, num2)
 
-    def addition(self):
-        add = lambda num1, num2: num1+num2
-        return add
+# These are the are operations
+add = np.vectorize(lambda x,y: x+y)    #np.vectorize returns a vectorized verson of the function. It takes array inputs and evaluates the function
+sub = np.vectorize(lambda x,y: x-y)
+mul = np.vectorize(lambda x,y: x*y) 
+div = np.vectorize(lambda x,y: x/y)
 
-    def subtraction(self):
-        sub = lambda num1,num2: num1-num2
-        return sub
-
-    def multiplicaion(self):
-        mul = lambda num1,num2: num1*num2
-        return mul
-
-    def division(self):
-        div = lambda num1,num2: num1/num2
-        return div
-
-class User_input(Calculator):
-    print("\n Select your operation type: ")
+def calculating():
+    print("\n Select your operation: ")
     print("A. Addition")
     print("B. Subtraction")
     print("C. Multiplication")
     print("D. Division")
-    print("E. Sqauring")
-    print("F. Square root")
+    
+    user_choice = input("\nEnter your option: ").lower()
+    num1 = int((input("\nEnter the first number: ")))
+    num2 = int((input("Enter the second number: ")))
 
-    user_choice = input("Enter your option: ").lower()
+    array1= np.array([num1])
+    array2= np.array([num2])
 
     if user_choice == "a":
+        operation = Calculator(add)
         
-
     elif user_choice == "b":
+        operation = Calculator(sub)
         
-
     elif user_choice == "c":
+        operation = Calculator(mul)
         
-
     elif user_choice == "d":
-        
+        operation = Calculator(div)
+
+    answer = operation.calculate(num1, num2)
+    print(f"The answer is: {answer}")
+
+calculating()       
 
 
 
-if __name__ == "__main__":
 
-    num1 = int(input("Enter the first number: "))
-    num2 = int(input("nter the second number: "))
+
+    
 
 
 
